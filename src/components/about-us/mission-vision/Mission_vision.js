e9b229305c5f9fc0f6f2599cad9bfb8bd831ae2c
DriWeApps <cto@driwe.in>
2025-12-05 13:20:13 +0530

    remove all errors

--- src/components/about-us/mission-vision/Mission_vision.js
+++ src/components/about-us/mission-vision/Mission_vision.js
@@ -1,3 +1,130 @@
+// import React, { useEffect } from "react";
+// import "./Mission_vision.css";
+// import Common_title from "../../common/common-title/Common_title";
+// import Common_button from "../../common/common-btn/Common_button";
+
+// const Mission_vision = () => {
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         entries.forEach((entry) => {
+//           if (entry.isIntersecting) {
+//             entry.target.classList.add("in-view");
+//           } else {
+//             entry.target.classList.remove("in-view");
+//           }
+//         });
+//       },
+//       { threshold: 0.3 }
+//     );
+
+//     document.querySelectorAll(".Mission_vision_sec").forEach((el) => {
+//       observer.observe(el);
+//     });
+
+//     return () => observer.disconnect();
+//   }, []);
+
+//   return (
+//     <>
+//       <section className="Mission_vision_sec">
+//         <div className="container">
+//           <Common_title
+//             specialtext={"Our Vision & Mission"}
+//             subtitle={
+//               "o make everyday travel and delivery easier, smarter, and more reliable for everyone"
+//             }
+//           />
+
+//           <div className="misvid_sec">
+//             <div className="row">
+//               <div className="col-lg-6 mb-3">
+//                 <div className="leftsec">
+//                   <div className="visionbox">
+//                     <div className="row">
+//                       <div className="col-3">
+//                         <div className="imgdiv">
+//                           <img
+//                             className="img"
+//                             src={
+//                               process.env.PUBLIC_URL +
+//                               "/assets/images/about/mission-vission/visionbox.png"
+//                             }
+//                           />
+//                         </div>
+//                       </div>
+//                       <div className="col-9 my-auto">
+//                         <div className="content-sec">
+//                           <p className="content">
+//                             At DriWE, our mission is to make every ride smooth, safe, 
+//                             and affordable for our customers. We believe in complete transparency, 
+//                             ensuring there are no hidden charges-only a clear, honest, 
+//                             and reliable ride experience.
+//                           </p>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                   <div className="visionbox">
+//                     <div className="row">
+//                       <div className="col-3">
+//                         <div className="imgdiv">
+//                           <img
+//                             className="img"
+//                             src={
+//                               process.env.PUBLIC_URL +
+//                               "/assets/images/about/mission-vission/missionbox.png"
+//                             }
+//                           />
+//                         </div>
+//                       </div>
+//                       <div className="col-9 my-auto">
+//                         <div className="content-sec">
+//                           <p className="content">
+//                             Our vision is to become the world’s best and safest ride-hailing application, 
+//                             setting new standards in reliability, service quality, and customer experience.
+//                           </p>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-lg-6 mb-3">
+//                 <div className="banner_div">
+//                   <img
+//                     className="banner"
+//                     src={
+//                       process.env.PUBLIC_URL +
+//                       "/assets/images/about/mission-vission/banner.png"
+//                     }
+//                   />
+
+//                   <div className="overlay-content">
+//                     <h6 className="ourh1">
+//                       Our Purpose at <br></br>DriWE
+//                     </h6>
+//                     <p className="wep">
+//                       We’re here to reshape the way cities move — making
+//                       everyday rides and on-demand deliveries fast, safe, and
+//                       effortlessly connected..
+//                     </p>
+//                     <p className="havep">Have questions or need support?</p>
+//                     <Common_button name={"Contact us"} link={"/contact-us"} />
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Mission_vision;
+
+
 import React, { useEffect } from "react";
 import "./Mission_vision.css";
 import Common_title from "../../common/common-title/Common_title";
@@ -32,20 +159,25 @@ const Mission_vision = () => {
           <Common_title
             specialtext={"Our Vision & Mission"}
             subtitle={
-              "o make everyday travel and delivery easier, smarter, and more reliable for everyone"
+              "To make everyday travel and delivery easier, smarter, and more reliable for everyone"
             }
           />
 
           <div className="misvid_sec">
-            <div className="row">
-              <div className="col-lg-6 mb-3">
+            <div className="row g-4">
+
+              {/* LEFT SIDE */}
+              <div className="col-lg-6">
                 <div className="leftsec">
+
+                  {/* VISION BOX 1 */}
                   <div className="visionbox">
-                    <div className="row">
+                    <div className="row align-items-center">
                       <div className="col-3">
                         <div className="imgdiv">
                           <img
                             className="img"
+                            alt="vision"
                             src={
                               process.env.PUBLIC_URL +
                               "/assets/images/about/mission-vission/visionbox.png"
@@ -53,24 +185,26 @@ const Mission_vision = () => {
                           />
                         </div>
                       </div>
-                      <div className="col-9 my-auto">
-                        <div className="content-sec">
-                          <p className="content">
-                            At DriWE, our mission is to make every ride smooth, safe, 
-                            and affordable for our customers. We believe in complete transparency, 
-                            ensuring there are no hidden charges-only a clear, honest, 
-                            and reliable ride experience.
-                          </p>
-                        </div>
+
+                      <div className="col-9">
+                        <p className="content">
+                          At DriWE, our mission is to make every ride smooth,
+                          safe, and affordable. We believe in complete
+                          transparency—no hidden charges, only reliable ride
+                          experiences.
+                        </p>
                       </div>
                     </div>
                   </div>
+
+                  {/* VISION BOX 2 */}
                   <div className="visionbox">
-                    <div className="row">
+                    <div className="row align-items-center">
                       <div className="col-3">
                         <div className="imgdiv">
                           <img
                             className="img"
+                            alt="mission"
                             src={
                               process.env.PUBLIC_URL +
                               "/assets/images/about/mission-vission/missionbox.png"
@@ -78,22 +212,26 @@ const Mission_vision = () => {
                           />
                         </div>
                       </div>
-                      <div className="col-9 my-auto">
-                        <div className="content-sec">
-                          <p className="content">
-                            Our vision is to become the world’s best and safest ride-hailing application, 
-                            setting new standards in reliability, service quality, and customer experience.
-                          </p>
-                        </div>
+
+                      <div className="col-9">
+                        <p className="content">
+                          Our vision is to become the world’s safest and most
+                          reliable ride-hailing platform—setting new standards
+                          for safety, trust, and customer experience.
+                        </p>
                       </div>
                     </div>
                   </div>
+
                 </div>
               </div>
-              <div className="col-lg-6 mb-3">
+
+              {/* RIGHT SIDE BANNER */}
+              <div className="col-lg-6">
                 <div className="banner_div">
                   <img
                     className="banner"
+                    alt="banner"
                     src={
                       process.env.PUBLIC_URL +
                       "/assets/images/about/mission-vission/banner.png"
@@ -101,19 +239,21 @@ const Mission_vision = () => {
                   />
 
                   <div className="overlay-content">
-                    <h6 className="ourh1">
-                      Our Purpose at <br></br>DriWE
-                    </h6>
+                    <h6 className="ourh1">Our Purpose at <br /> DriWE</h6>
+
                     <p className="wep">
-                      We’re here to reshape the way cities move — making
-                      everyday rides and on-demand deliveries fast, safe, and
-                      effortlessly connected..
+                      We’re here to reshape city movement — making rides and
+                      on-demand deliveries fast, safe, and effortlessly
+                      connected.
                     </p>
+
                     <p className="havep">Have questions or need support?</p>
+
                     <Common_button name={"Contact us"} link={"/contact-us"} />
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
@@ -123,3 +263,4 @@ const Mission_vision = () => {
 };
 
 export default Mission_vision;
+
